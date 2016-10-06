9ffa2f9f399eaf498fb266f0812f8c1c99df4011
Komaron James <kjames@jibe.com>
2016-10-05 23:08:20 -0400

    Initial Commit

---
+++ lib/ddg/wrapper.rb
@@ -0,0 +1,7 @@
+require "ddg/wrapper/version"
+
+module Ddg
+  module Wrapper
+    # Your code goes here...
+  end
+end
---
+++ lib/ddg/wrapper/version.rb
@@ -0,0 +1,5 @@
+module Ddg
+  module Wrapper
+    VERSION = "0.1.0"
+  end
+end
