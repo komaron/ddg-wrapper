4ca68225e0b13fc9fb2734f32b01bcce42078cdb
Komaron James <kjames@jibe.com>
2016-10-06 21:57:01 -0400

    Fixing tab spaces

--- lib/ddg-wrapper/client.rb
+++ lib/ddg-wrapper/client.rb
@@ -1,35 +1,35 @@
 module DDG
-	module Wrapper
-		class Client
-			attr_accessor :url
-			API_URL = 'http://api.duckduckgo.com/'
+  module Wrapper
+    class Client
+      attr_accessor :url
+      API_URL = 'http://api.duckduckgo.com/'
 
-			def initialize
-				@url = API_URL
-			end
+      def initialize
+        @url = API_URL
+      end
 
-			def query(query)
-				params = build_params(query)
-				data = get_data(params)
-				data
-			end
+      def query(query)
+        params = build_params(query)
+        data = get_data(params)
+        data
+      end
 
-			def build_params(query, format='json',pretty='1')
-				params = {
-					q: 			query,
-					format: format,
-					pretty: pretty
-				}
-				params
-			end
+      def build_params(query, format='json',pretty='1')
+        params = {
+          q: 			query,
+          format: format,
+          pretty: pretty
+        }
+        params
+      end
 
-			def get_data(params)
-				uri = URI(url)
-				uri.query = URI.encode_www_form(params)
-				res = Net::HTTP.get_response(uri)
+      def get_data(params)
+        uri = URI(url)
+        uri.query = URI.encode_www_form(params)
+        res = Net::HTTP.get_response(uri)
 
-				JSON.parse(res.body)
-			end
-		end
-	end
+        JSON.parse(res.body)
+      end
+    end
+  end
 end
